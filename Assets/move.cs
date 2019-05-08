f01a27830214db8af42c87591e30e578cca4aa8d
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-05-08 12:25:11 +0900

    0508

---
+++ Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float speed = 20f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 mpos = Input.mousePosition;
+        Vector3 wpos = Camera.main.ScreenToWorldPoint(mpos);
+        Vector3 move = wpos - transform.position;
+        rb.velocity = move.normalized * speed;
+    }
+}
+
+
+
+
